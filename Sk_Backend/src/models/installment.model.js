13a6eaf75f60aa5d9c1eea242929c5456c8b60a5
Tushar Rawat <usharawat901@gmail.com>
2025-02-21 18:46:38 +0530

    Adding backend to main

---
+++ Sk_Backend/src/models/installment.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const installmentSchema = new mongoose.Schema({
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
+  installmentName: { type: String, required: true },
+  amount: { type: Number, required: true },
+  date: { type: String, required: true }, // Format: dd-mm-yyyy
+}, { timestamps: true });
+
+export default mongoose.model("Installment", installmentSchema);

fd4d335e6f0df1978ba0de7cb6020553ac0516b5
Tushar Rawat <usharawat901@gmail.com>
2025-06-24 11:59:26 +0530

    Change the URL OF BACKEND test deployed

--- Frontend/frontend/config.js
+++ Frontend/frontend/config.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = "https://sk-edutech-backend.onrender.com";
+const API_BASE_URL = "https://sk-edutech.onrender.com";
 export default API_BASE_URL;
+// http://localhost:5000
+// https://sk-edutech-backend.onrender.com

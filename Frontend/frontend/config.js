3136a900d12a5548a40c0efed0e75a33e6d44189
Tushar Rawat <usharawat901@gmail.com>
2025-06-24 23:03:50 +0530

    add toml file and chagne in the config.js

--- Frontend/frontend/config.js
+++ Frontend/frontend/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = "https://sk-edutech.onrender.com";
 export default API_BASE_URL;
-// http://localhost:5000
-// https://sk-edutech-backend.onrender.com
+// http://localhost:8000
+// https://sk-edutech.onrender.com

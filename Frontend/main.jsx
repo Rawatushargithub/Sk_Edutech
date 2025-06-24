7a11a50a273eb0675c3e1d6dee1c50deae36d910
Tushar Rawat <usharawat901@gmail.com>
2025-06-24 15:45:20 +0530

    main.jsx

--- Frontend/main.jsx
+++ Frontend/main.jsx
@@ -1,5 +1,5 @@
 import {  BrowserRouter } from "react-router-dom";
-import {ReactDOM,  createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 

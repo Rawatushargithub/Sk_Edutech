96ad7439fa4aa5b43a083b86a4f88d6a99af818a
Tushar Rawat <usharawat901@gmail.com>
2025-08-08 10:23:23 +0530

    fix:admin reject in wallet approval.

--- Backend/backend/Admin_Backend/routes/admin.routes.js
+++ Backend/backend/Admin_Backend/routes/admin.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { approveTransaction, getAllTransactions } from "../controllers/wallet/adminWallet.controller.js";
+import { approveTransaction, getAllTransactions ,rejectTransaction} from "../controllers/wallet/adminWallet.controller.js";
 const router = express.Router();
 // Approve transaction
 router.post('/transactions/:transactionId/approve', approveTransaction);
 
 router.get("/transactions" , getAllTransactions)
+router.post("/transactions/:transactionId/reject", rejectTransaction)
   export default router;
